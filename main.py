330908682971fbab3adab83de468fc0cb65dcd71
mary-dracheva <mary.dracheva@gmail.com>
2022-09-11 23:56:27 +0300

    ml exerice

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 
 
 if __name__ == '__main__':
-
-
     """batch_size = 64
     training_data = [[0, 0], [2, 4], [3, 6]]
-    test_data = [(4, 8), (5, 10), (6, 12)]
-
-    
+    test_data = [(4, 8), (5, 10), (6, 12)]    
     test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
     """
-
-
-
-
